22e13c453c4eb6c2036a5072b0e7080ec1929310
Harsimran Saini <saini99@yorku.ca>
2023-11-07 14:09:51 -0500

    new project

---
+++ EECS4413Project/src/main/java/Indigo/EECS4413Project/Eecs4413ProjectApplication.java
@@ -0,0 +1,13 @@
+package Indigo.EECS4413Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eecs4413ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eecs4413ProjectApplication.class, args);
+	}
+
+}
---
+++ EECS4413Project/src/main/java/Indigo/EECS4413Project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package Indigo.EECS4413Project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Eecs4413ProjectApplication.class);
+	}
+
+}
