bf104954e9445cf9f4d5cc680f5bf21bb97df460
aisha895 <aisha895@my.yorku.ca>
2023-11-11 15:32:09 -0500

    Seller class

--- EECS4413Project/src/main/java/com/auction/DutchAuctionDAO.java
+++ EECS4413Project/src/main/java/com/auction/DutchAuctionDAO.java
@@ -1,5 +1,10 @@
 package com.auction;
 
 public class DutchAuctionDAO extends Auction implements AuctionInterface{
+	  
+	public DutchAuctionDAO() {
+	
+	     
+	    }
 
 }
---
+++ EECS4413Project/src/main/java/com/auction/Seller.java
@@ -0,0 +1,5 @@
+package com.auction;
+
+public class Seller {
+
+}
