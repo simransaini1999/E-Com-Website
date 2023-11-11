7e312ff131e2c35db85d21824cdc888429567fc5
aisha895 <aisha895@my.yorku.ca>
2023-11-11 15:17:10 -0500

    forward auction

--- EECS4413Project/src/main/java/com/auction/ForwardAuctionDAO.java
+++ EECS4413Project/src/main/java/com/auction/ForwardAuctionDAO.java
@@ -6,8 +6,8 @@ public class ForwardAuctionDAO extends Auction implements AuctionInterface{
 	this.setprice(100);
 
 
-	public void settingBid(User user, int price) {
-		if (price > this.getPrice()) {
+	public void settingBid(User user, int bidAmount) {
+		if (bidAmount > this.getPrice()) {
 			this.setBidder(user); 
 		}
 	}
