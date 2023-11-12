ec444823d2bdc9c26157493bc347a2156f0cb4aa
aisha895 <aisha895@my.yorku.ca>
2023-11-11 20:24:50 -0500

    AuctionDAO

--- EECS4413Project/src/main/java/com/auction/AuctionDAO.java
+++ EECS4413Project/src/main/java/com/auction/AuctionDAO.java
@@ -2,6 +2,21 @@ package com.auction;
 
 public class AuctionDAO {
 	
+	Auction auction;
 	
+	public Auction create(Item item) {
+		
+		if(item.getAuctionType().equals("Dutch Auction")) {
+			auction = new DutchAuctionDAO();
+			
+		}
+		else {
+			auction = new ForwardAuctionDAO();
+		}
+		
+		auction.setItem(item);
+		
+		return auction;
+	}
 
 }
