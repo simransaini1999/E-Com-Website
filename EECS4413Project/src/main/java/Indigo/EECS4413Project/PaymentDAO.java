390dfdb565937223a8db1005e4980530198a62ec
hashi11002 <harshilkumar12@gmail.com>
2023-11-13 23:22:24 -0500

    Made payment

--- EECS4413Project/src/main/java/Indigo/EECS4413Project/PaymentDAO.java
+++ EECS4413Project/src/main/java/Indigo/EECS4413Project/PaymentDAO.java
@@ -9,15 +9,50 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpSession;
 @Repository
 public class PaymentDAO {
+
 	@Autowired
 	UserRepository userrepo;
+	
+	@Autowired
+	ItemDAO itemDAO;
+	
+	@Autowired
+	HttpSession session;
+	
+	@Autowired
+	ServletContext context;
 
 	
-	
-	public User getUserbyId( int id) {
-		return userrepo.findByID(id);
+	public Item getItem() {
+		Item item = itemDAO.getItemByName((String)session.getAttribute("itemName"));
+		return item;
 	}
+	
+	public User getDutchUser() {
+		User user = userrepo.findByID( (int) context.getAttribute("highestDutchBidder"));
+		return user;
+	}
+	public int getDutchBidPrice() {
+		return (int) context.getAttribute("dutchBidAmount");
+	}
+	
+	public User getForwardUser() {
+		User user = userrepo.findByID( (int) context.getAttribute("highestForwardBidder"));
+		return user;
+	}
+	public int getForwardBidPrice() {
+		return (int) context.getAttribute("forwardBidAmount");
+	}
+	
+	
+	
+	
+	
+	
 
 }
